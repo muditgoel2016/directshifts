1e367c267005db7023d110824007d03455382b01
Mudit Goel <mudit.goel@scribie.com>
2023-03-28 12:10:40 +0530

    removed my PAT as has expired

--- src/config/github.js
+++ src/config/github.js
@@ -1,7 +1,7 @@
 const githubConfig = {
     REPO_OWNER: "facebook",
     REPO: "react",
-    PAT: "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" //my personal access token
+    PAT: "" //my personal access token
   };
 
   export default githubConfig;
