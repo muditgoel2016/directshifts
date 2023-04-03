c95c3717ebc38c1a140ac59711320889daaba4f5
Mudit Goel <mudit.goel@scribie.com>
2023-04-03 12:51:52 +0530

    removed PAT

--- src/config/github.js
+++ src/config/github.js
@@ -1,7 +1,7 @@
 const githubConfig = {
     REPO_OWNER: "facebook",
     REPO: "react",
-    PAT: "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" //my personal access token
+    PAT: "" //my personal access token
 };
 
 export default githubConfig;
